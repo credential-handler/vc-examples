0d4fb429dcb31644d9fbf3dd6c6930080e57eb5e
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2023-02-22 12:16:41 -0500

    Add `.gitignore` and `lint` script.

--- credentials/index.js
+++ credentials/index.js
@@ -1,9 +1,9 @@
 /*!
  * Copyright (c) 2023 Digital Bazaar, Inc. All rights reserved.
  */
-import path from 'node:path';
 import {fileURLToPath} from 'node:url';
+import path from 'node:path';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-export const credentialsDir = path.resolve(__dirname, "credentials");
+export const credentialsDir = path.resolve(__dirname, 'credentials');
