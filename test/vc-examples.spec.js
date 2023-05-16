8c6ac80a1d765e1b78980b55bc415e40e2ef71f5
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2023-05-16 15:53:30 -0400

    Add test.

--- test/vc-examples.spec.js
+++ test/vc-examples.spec.js
@@ -1,16 +1,21 @@
 /*!
  * Copyright (c) 2023 Digital Bazaar, Inc. All rights reserved.
  */
+import {contextsDir, credentialsDir} from '../lib/index.js';
 import chai from 'chai';
-import {credentialsDir} from '../lib/index.js';
 import fs from 'node:fs';
 
 const should = chai.should();
 
-describe('credentialsDir', () => {
+describe('credentialsDir & contextsDir', () => {
   it('should exist path to credentials directory.', async () => {
     should.exist(credentialsDir);
     credentialsDir.should.be.a('string');
     fs.existsSync(credentialsDir).should.be.true;
+  });
+  it('should exist path to contexts directory.', async () => {
+    should.exist(contextsDir);
+    contextsDir.should.be.a('string');
+    fs.existsSync(contextsDir).should.be.true;
   });
 });
