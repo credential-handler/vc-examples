181857e9666991848a769a29fcaf931d2e6abf58
Benjamin Young <byoung@digitalbazaar.com>
2024-10-31 10:53:44 -0400

    Add npm run check script for safe mode.
    Uses `jsonld.expand()` safe mode to find undefined terms.
    
    It uses the `jsonld-document-loader` to map locally defined contexts
    to their future publication URLs allowing local testing of in progress
    context changes without the need to first publish the context or use
    temporary URLs for the contexts and avoids the temptation to use
    inline contexts.

---
+++ test/safe-mode-check.js
@@ -0,0 +1,49 @@
+import {contextsDir, credentialsDir} from '../lib/index.js';
+import EleventyFetch from '@11ty/eleventy-fetch';
+import fs from 'node:fs';
+import {glob} from 'glob';
+import jsonld from 'jsonld';
+import {JsonLdDocumentLoader} from 'jsonld-document-loader';
+
+const jdl = new JsonLdDocumentLoader();
+
+// Loop all local context files and add them as static contexts at the base URL
+const baseUrl = 'https://contexts.vcplayground.org/examples/';
+const paths = await glob([`${contextsDir}/**/*.json`],
+  {stat: true, withFileTypes: true});
+const timeSortedFiles = paths
+  .sort((a, b) => a.mtimeMs - b.mtimeMs)
+  .map(path => path.fullpath());
+
+const latestLocalContexts = new Map(timeSortedFiles.map(path => {
+  const parts = path.split('/');
+  return [parts[parts.length - 2], parts[parts.length - 1]];
+}));
+
+latestLocalContexts.forEach((file, dir) => {
+  const context = JSON.parse(
+    fs.readFileSync(`${contextsDir}/${dir}/${file}`));
+  const url = `${baseUrl}${dir}/${file}`;
+  jdl.addStatic(url, context);
+});
+jdl.setProtocolHandler({protocol: 'https',
+  handler: {
+    async get({url}) {
+      return EleventyFetch(url, {duration: '1d', type: 'json'});
+    }
+  }});
+const loader = jdl.build();
+jsonld.documentLoader = loader;
+
+const credentialPaths = await glob([`${credentialsDir}/**/credential.json`]);
+credentialPaths.forEach(async credentialPath => {
+  const credential = JSON.parse(await fs.readFileSync(credentialPath));
+  try {
+    await jsonld.expand(credential, {safe: true});
+    console.log('👍 All terms correctly defined in',
+      credentialPath);
+  } catch(err) {
+    console.log('😢 Errors found in', credentialPath);
+    console.dir(err, {depth: 5});
+  }
+});
