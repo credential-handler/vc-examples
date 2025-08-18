efe59683618a1ff88469fe0e1098877fcc2d2d79
Benjamin Young <byoung@digitalbazaar.com>
2025-08-18 17:18:09 -0400

    Use 11ty for publishing static files.

---
+++ eleventy.config.js
@@ -0,0 +1,15 @@
+/**!
+ * Copyright 2025 Digital Bazaar, Inc.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+export default async function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('contexts/');
+  eleventyConfig.addPassthroughCopy('credentials/');
+  return {
+    dir: {
+      input: 'src/'
+    }
+  };
+}
